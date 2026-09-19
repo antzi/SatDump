999bf056a5c9e39898e8d8eff453067dd1530b1a
Anastas Giokov <agiokov@gmail.com>
2026-09-19 11:11:40 +0300

    Update module_metop_instruments.cpp
    Skipping MHS processing if lines are less than 3

--- plugins/noaa_metop_support/metop/module_metop_instruments.cpp
+++ plugins/noaa_metop_support/metop/module_metop_instruments.cpp
@@ -246,10 +246,17 @@ namespace metop
                 nlohmann::json calib_coefs = loadJsonFile(resources::getResourcePath("calibration/MHS.json"));
                 if (calib_coefs.contains(sat_name))
                 {
-                    mhs_reader.calibrate(calib_coefs[sat_name]);
-                    mhs_products.set_calibration("noaa_mhs", mhs_reader.calib_out);
-                    for (int i = 0; i < 5; i++)
-                        mhs_products.set_channel_wavenumber(i, calib_coefs[sat_name]["wavenumber"][i]);
+                    if (mhs_reader.line >= 3)
+                    {
+                        mhs_reader.calibrate(calib_coefs[sat_name]);
+                        mhs_products.set_calibration("noaa_mhs", mhs_reader.calib_out);
+                        for (int i = 0; i < 5; i++)
+                            mhs_products.set_channel_wavenumber(i, calib_coefs[sat_name]["wavenumber"][i]);
+                    }
+                    else
+                    {
+                        logger->warn("Not enough MHS lines for calibration, skipping MHS processing");
+                    }
                 }
                 else
                     logger->warn("(MHS) Calibration data for " + sat_name + " not found. Calibration will not be performed");
